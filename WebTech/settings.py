247463667da147d4dbfb395d1697cb920c91e1ca
Sander Lintzen <s.f.a.lintzen@student.tue.nl>
2018-01-17 10:05:35 +0100

    fixed allowed hosts

--- WebTech/settings.py
+++ WebTech/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'iopi3sj+hdtkuzhe0ly!u!-*uhvmqlq$y*l!rr+z2w*7%1hqph'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['squashcie.pythonanywhere.com']
 
 
 # Application definition
