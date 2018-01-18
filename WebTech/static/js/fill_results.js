fb60fbd8c71ed960cb66bbfdc403001799279817
Sander Lintzen <s.f.a.lintzen@student.tue.nl>
2018-01-18 16:43:54 +0100

    trying the stuff

---
+++ WebTech/static/js/fill_results.js
@@ -0,0 +1,8 @@
+function resultButton() {
+    var x = document.getElementById("fillScore");
+    if (x.style.display === "none") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
