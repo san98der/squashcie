500fbc1f3a1f88029861667f9309b314203d9a21
Sander Lintzen <s.f.a.lintzen@student.tue.nl>
2018-01-18 16:39:21 +0100

    deleted fill results

--- WebTech/static/js/fill_results.js
+++
@@ -1,8 +0,0 @@
-function resultButton() {
-    var x = document.getElementById("fillScore");
-    if (x.style.display === "none") {
-        x.style.display = "none";
-    } else {
-        x.style.display = "block";
-    }
-}
